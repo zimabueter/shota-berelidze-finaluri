a89827e2b36e256f91503a7e682e1fb469a23edc
zimabueter <44813983+zimabueter@users.noreply.github.com>
2021-07-06 20:53:42 +0400

    Delete src directory

--- src/main/java/com/example/shotaberelidze1/repository/AttachmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.shotaberelidze1.repository;
-
-import com.example.shotaberelidze1.model.Attachment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
-}
--- src/main/java/com/example/shotaberelidze1/repository/AuthRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.shotaberelidze1.repository;
-
-import com.example.shotaberelidze1.model.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface AuthorRepository extends JpaRepository<Author, Long> {
-}
